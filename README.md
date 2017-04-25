fb6a11244cfb3e3677dae6cc593128d32798eee6
bram <bramnagtegals51@gmail.com>
2017-04-25 20:46:50 +0200

    read.md added

---
+++ README.md
@@ -0,0 +1,29 @@
+# terraform-cloudatcost
+cloudatcost provider terraform
+
+## References
+https://github.com/cloudatcost/api
+github.com/masayukioguni/go-cloudatcost/cloudatcost
+github.com/Blackturtle123/go-cloudatcost/cloudatcost
+
+## Example
+provider "cloudatcost" {
+  api_key     = "key"
+  login       = "email"
+}
+
+resource "cloudatcost_machine" "servera" {
+  "cpu"="1",
+  "ram"="512",
+  "storage"="10",
+  "os"="26",
+  "datacenter"="Developer-DC-2",
+}
+
+resource "cloudatcost_machine" "serverb" {
+  "cpu"="1",
+  "ram"="512",
+  "storage"="20",
+  "os"="26",
+  "datacenter"="Developer-DC-2",
+}
---
+++ main.go
@@ -0,0 +1,123 @@
+
+package main
+
+import (
+	//"strconv"
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/BlackTurtle123/go-cloudatcost/cloudatcost"
+)
+
+func main() {
+	opts := plugin.ServeOpts{
+		ProviderFunc: Provider,
+	}
+	plugin.Serve(&opts)
+}
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{ // Source https://github.com/hashicorp/terraform/blob/v0.6.6/helper/schema/provider.go#L20-L43
+		Schema:        providerSchema(),
+		ResourcesMap:  providerResources(),
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+func providerSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"api_key": &schema.Schema{
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "API Key used to authenticate with the service provider",
+		},
+		"login": &schema.Schema{
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "login used to authenticate with the service provider",
+		},
+	}
+}
+
+func providerResources() map[string]*schema.Resource {
+	return map[string]*schema.Resource{
+		"cloudatcost_machine": &schema.Resource{
+			SchemaVersion: 1,
+			Create:        createFunc,
+			Read:          readFunc,
+			Update:        updateFunc,
+			Delete:        deleteFunc,
+			Schema: map[string]*schema.Schema{ // List of supported configuration fields for your resource
+				"storage": &schema.Schema{
+					Type:     schema.TypeString,
+					Required: true,
+				},
+				"datacenter": &schema.Schema{
+					Type:     schema.TypeString,
+					Required: true,
+				},
+				"os": &schema.Schema{
+					Type:     schema.TypeString,
+					Required: true,
+				},
+				"cpu": &schema.Schema{
+					Type:     schema.TypeString,
+					Required: true,
+				},
+				"ram": &schema.Schema{
+					Type:     schema.TypeString,
+					Required: true,
+				},
+			},
+		},
+	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+
+
+	return  cloudatcost.NewClient(&cloudatcost.Option{Login: d.Get("login").(string), Key: d.Get("api_key").(string)})
+}
+
+func createFunc(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*cloudatcost.Client)
+	fmt.Println(client)
+
+	_, _, err := client.CloudProService.Create(&cloudatcost.CreateServerOptions{
+		Cpu: d.Get("cpu").(string),
+		Ram:  d.Get("ram").(string),
+		Storage:  d.Get("storage").(string),
+		OS:  d.Get("os").(string),
+		Datacenter: d.Get("datacenter").(string) },
+	)
+
+	if err != nil {
+		return err
+	}
+
+	listservers,_, err := client.ServersService.List()
+
+	serverLength := len(listservers)
+	server := listservers[serverLength-1]
+	if err != nil {
+		return err
+	}
+
+
+	//d.SetId(strconv.Itoa(server.Sid))
+	d.SetId(server.Sid)
+	return nil
+}
+
+func readFunc(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func updateFunc(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+
+func deleteFunc(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
